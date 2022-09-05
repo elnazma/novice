febe50053a2fc254506f1afb4318a9fa37d51f40
nzlxxmld <nazlitaelva@gmail.com>
2022-09-05 11:53:39 -0700

    pesan text

--- novice/03-05/APP/main.py
+++ novice/03-05/APP/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
 
 app = Flask(__name__)
@@ -18,32 +18,45 @@ def index():
         query = f"insert into buah (nama, detail) values ('{nama}', '{detail}')"
         curs.execute(query)
         conn.commit()
-        
-
-        # print(20*"=")
-        # print(nama)
-        # print(detail)
-        # print(20*"=")
-
-    print(request.method)
-    query=f"select * from buah"
+        print(request.method)
+    query = f"select * from buah"
     curs.execute(query)
     data = curs.fetchall()
+    return render_template("index.html", context=data)
+
+@app.route("/detail/<buah_id>")
+def detail(buah_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="7mei2004")
+
+    curs = conn.cursor()
+    query = f"select * from buah where id = {buah_id}"
+    curs.execute(query)
+    data = curs.fetchone()
+    conn.commit()
     curs.close()
     conn.close()
-    return render_template("index.html", context=data)
-    
+    print(data)
+    return render_template("detail.html", context=data)
 
-    data = ["alpukat", "mangga", "pepaya"]
-    return render_template("index.html", context=data)
+@app.route("/delete/<buah_id>")
+def delete(buah_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="7mei2004")
 
-# @app.route("/post")
-# def post():
-#     return render_template("post.html")
-
-# @app.route("/about")
-# def about():
-#     return render_template("about.html")
+    curs = conn.cursor()
+    query = f"delete from buah where id = {buah_id}"
+    curs.execute(query)
+    conn.commit()
+    curs.close()
+    conn.close()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run()
