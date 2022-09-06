a97ba5316ab4f1a277f914f427486349ed0ac90c
nzlxxmld <nazlitaelva@gmail.com>
2022-09-06 11:44:47 -0700

    pesan text

--- novice/03-05/APP/main.py
+++ novice/03-05/APP/main.py
@@ -67,7 +67,7 @@ def delete(buah_id):
     conn.close()
     return redirect("/")
 
-@app.route("/update/<buah_id>")
+@app.route("/update/<buah_id>", methods=["GET", "POST"])
 def update(buah_id):
     conn = psycopg2.connect(
         host="localhost",
@@ -76,19 +76,23 @@ def update(buah_id):
         password="7mei2004"
     )
     curs = conn.cursor()
-    
-    namaLama = 'alpukat'
-    namaBaru = 'strawberry'
-    detailBaru = 'merah'
-
-    query = f"update buah set nama='{namaBaru}', detail='{detailBaru}' where nama ='{namaLama}'"
+    if request.method == "POST":
+        nama = request.form.get("nama")
+        detail = request.form.get("detail")
+        query = f"update buah set nama = '{nama}', detail = '{detail}' where id = {buah_id}"
+        curs.execute(query)
+        conn.commit()
+        return redirect("/")
+        # print(nama, detail)
+    query = f"select * from buah where id = {buah_id}"
     curs.execute(query)
-    conn.commit()
-    print("data masuk")
+    data = curs.fetchone()
+    # conn.commit()
+    curs.close()
+    conn.close()
+    return render_template("update.html", context=data)
 
-    return redirect("/")
-
-
+    
 
 if __name__ == "__main__":
     app.run()
